b951a2f75b20a1a08714fc2c8c1c6af65c57913e
Ryfd55 <gubardin1984@mail.ru>
2022-05-21 22:50:11 +0400

    1

--- app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.widget.TextView
 
 class EncryptFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_encrypt,container, false)
+        val view = inflater.inflate(R.layout.fragment_encrypt,container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+        encryptedView.text = message.reversed()
+        return view
     }
 }
